77738a6ae9766c705f875ad17fcbf4192c859698
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-09-23 14:21:40 -0700

    Add FlaskDebug switch parameter to run script

--- run.ps1
+++ run.ps1
@@ -1,6 +1,13 @@
 param (
-    $AppPath = "$PSScriptRoot\glue\app.py"
+    $AppPath = "$PSScriptRoot\glue\app.py",
+
+    [switch]
+    $FlaskDebug
 )
+
+if ($FlaskDebug) {
+    $env:FLASK_DEBUG = 1
+}
 
 $env:FLASK_APP = $AppPath
 
