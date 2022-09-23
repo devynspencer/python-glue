e94165c115ec00154e77cf2ea9b4e34bb88da6a8
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-09-23 14:36:22 -0700

    Add basic /about endpoint

--- glue/app.py
+++ glue/app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask
 
 
@@ -7,6 +8,17 @@ app = Flask(__name__)
 @app.route("/")
 def handle_root():
     return "Everything works, you're incredible."
+
+
+@app.route("/about")
+def handle_about():
+    about = {
+        "root_path": app.root_path,
+        "template_path": app.template_folder,
+        "env": app.env,
+    }
+
+    return json.dumps(about)
 
 
 if __name__ == "__main__":
