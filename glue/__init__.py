cae89861b479b61d88e110c113d2462eec89d2d7
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-09-24 14:31:15 -0700

    Add application factory function
    https://flask.palletsprojects.com/en/2.2.x/tutorial/factory/

---
+++ glue/__init__.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from flask import Flask, render_template
+
+
+# https://flask.palletsprojects.com/en/2.2.x/tutorial/factory/
+
+
+def create_app(config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    # Enable development mappings and specify database location
+    app.config.from_mapping(
+        SECRET_KEY="dev", DATABASE=Path(app.instance_path, "glue.db")
+    )
+
+    # Load test configuration unless an override specified when running create_app
+    if config is None:
+        app.config.from_pyfile("config.py", silent=True)
+
+    else:
+        app.config.from_mapping(config)
+
+    # Ensure instance directory exists
+    try:
+        Path(app.instance_path).mkdir()
+
+    # TODO: This seems janky
+    except OSError as e:
+        print(f"Error with app instance_path {app.instance_path}:\n{e}")
+        pass
+
+    # TODO: Move these to a routes file
+    @app.route("/")
+    def handle_root():
+        return render_template("home.html")
+
+    @app.route("/about")
+    def handle_about():
+        about = {
+            "root_path": app.root_path,
+            "template_path": app.template_folder,
+            "env": app.env,
+        }
+
+        return render_template("about.html", msg=about, title="About")
+
+    return app
