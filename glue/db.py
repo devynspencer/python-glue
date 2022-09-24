db1e4358bd25fe1f204d5e5b3881af2a590f7060
Devyn Spencer <devynspencer@users.noreply.github.com>
2022-09-24 14:31:45 -0700

    Add basic database functionality
    Using application config values so the database is available to an app
    created with application factory.

---
+++ glue/db.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+from flask import current_app, g
+import click
+
+# https://flask.palletsprojects.com/en/2.2.x/tutorial/database/
+
+
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect(
+            current_app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES
+        )
+
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop("db", None)
+
+    if db is not None:
+        db.close()
